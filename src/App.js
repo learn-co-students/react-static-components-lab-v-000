b2993b331c946ffe5715f9d2ca1341371a2451b5
edb-c <edb.cofresi@gmail.com>
2019-01-09 13:40:11 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CatComponent from './CatComponent.js';
 import { EinsteinQuoteComponent } from './EinsteinQuoteComponent';
 import MouseComponent from './MouseComponent';
 
+
 class App extends Component {
 	render() {
 		// your code in the return statement below!
