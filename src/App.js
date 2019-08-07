daa83a2c0096c8a2f4080d52173054b8e8dd333f
Mohammed Noori <kmanoorib@gmail.com>
2019-08-07 18:19:12 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ import MouseComponent from './MouseComponent';
 class App extends Component {
 	render() {
 		// your code in the return statement below!
+		// all I had to do was put <CatOmponent />
+		// <EinsteinQuoteComponent />
+		// and <Mouse Componant />
+		// to pass the tests 
+		//div className="App" was initially empty
 		return (
 			<div className="App">
+			  <CatComponent />
+			  <EinsteinQuoteComponent />
+			  <MouseComponent />
 			</div>
 		);
 	}
